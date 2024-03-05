0431d73e48b5615207009b83cffb9f8d782fb565
huihuangzhang <1046604957@qq.com>
2024-03-05 23:58:21 +0800

    feat: add vscode fold&unfold key binding (#4)
    Co-authored-by: zhanghuihuang <zhanghuihuang@bytedance.com>

--- config/nvim/lua/vscode/keymap.lua
+++ config/nvim/lua/vscode/keymap.lua
@@ -3,4 +3,33 @@ vim.keymap.set('n', 'gI', function()
   require('vscode-neovim').call('editor.action.goToImplementation')
 end, { noremap = true, silent = true })
 
+-- fold keymap
+vim.keymap.set('n', 'zM', function()
+  require('vscode-neovim').call('editor.foldAll')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', 'zR', function()
+  require('vscode-neovim').call('editor.unfoldAll')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', 'zc', function()
+  require('vscode-neovim').call('editor.fold')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', 'zC', function()
+  require('vscode-neovim').call('editor.foldRecursively')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', 'zo', function()
+  require('vscode-neovim').call('editor.unfold')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', 'zO', function()
+  require('vscode-neovim').call('editor.unfoldRecursively')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', 'za', function()
+  require('vscode-neovim').call('editor.toggleFold')
+end, { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap('n', '<leader>,', 'A,<Esc>', { noremap = true, silent = true })
