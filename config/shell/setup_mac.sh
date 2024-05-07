95b3355a34f8a48dab93868edd7f6b984aca8ff6
zhang <1046604957@qq.com>
2024-05-07 20:12:19 +0800

    feat: mac install zinit raycast nvm

--- config/shell/setup_mac.sh
+++ config/shell/setup_mac.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-function setup_fzf() {
-    brew install fzf
-    
-    $(brew --prefix)/opt/fzf/intall --all
+set -o errexit  
+set -o pipefail 
+set -o nounset  
+
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
 }
 
 function setup_brew() {
+    if command_exists brew; then
+        echo "'brew' is already installed."
+    fi
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
-# install nvm
-# install gvm
-# install zinit
+function setup_fzf() {
+    if command_exists fzf; then
+        echo "'fzf' is already installed."
+    fi
 
-# grep
-export GREP_OPTIONS='--color=always'
-export GREP_COLOR='1;35;40'
+    brew install fzf
+    $(brew --prefix)/opt/fzf/intall --all
+}
+
+function install_raycast() {
+    brew install --cask raycast
+}
+
+function install_zinit() {
+    bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+}
+
+function install_nvm() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+}
+
+# TODO:
+# install gvm
